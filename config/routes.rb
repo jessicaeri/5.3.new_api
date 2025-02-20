8184edc1267abaf42c3728bfbba140cdb3042628
jessicaeri <jess.eri0926@gmail.com>
2025-02-20 11:29:19 +1000

    edit controller name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/mbta_birds' => 'birds#index'
-  get '/mbta_birds/:id' => 'birds#show'
-  post '/mbta_birds' => 'birds#create'
-  patch '/mbta_birds/:id' => 'birds#update'
-  delete '/mbta_birds/:id' => 'birds#destroy'
+  get '/mbta_birds' => 'mbta_birds#index'
+  get '/mbta_birds/:id' => 'mbta_birds#show'
+  post '/mbta_birds' => 'mbta_birds#create'
+  patch '/mbta_birds/:id' => 'mbta_birds#update'
+  delete '/mbta_birds/:id' => 'mbta_birds#destroy'
 end
